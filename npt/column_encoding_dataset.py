4ff5f8f6e2dd05130cdfc23f3aa9cb3207b8fe5e
Huda AlGhussein <112384995+Huda-AlGhussein@users.noreply.github.com>
2024-03-19 11:53:57 +0400

    Update column_encoding_dataset.py

--- npt/column_encoding_dataset.py
+++ npt/column_encoding_dataset.py
@@ -13,13 +13,13 @@ from npt.datasets.cifar10 import CIFAR10Dataset
 from npt.datasets.concrete import ConcreteDataset
 from npt.datasets.debug import DebugDataset
 #from npt.datasets.forest_cover import ForestCoverClassificationDataset
-from npt.datasets.higgs import HiggsClassificationDataset
+#from npt.datasets.higgs import HiggsClassificationDataset
 from npt.datasets.image_utils import load_image_dataloaders
 from npt.datasets.income import IncomeDataset
 from npt.datasets.kick import KickDataset
 from npt.datasets.mnist import MNISTDataset
-from npt.datasets.poker_hand import PokerHandDataset
-from npt.datasets.protein import ProteinDataset
+#from npt.datasets.poker_hand import PokerHandDataset
+#from npt.datasets.protein import ProteinDataset
 from npt.datasets.yacht import YachtDataset
 from npt.datasets.cross_project_defect_df import CrossProjectDefectDataset
 from npt.utils.cv_utils import (
