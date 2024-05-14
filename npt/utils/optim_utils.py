465049ae425384153fcee9b0729723360315dbc3
Huda AlGhussein <112384995+Huda-AlGhussein@users.noreply.github.com>
2024-05-14 12:39:32 +0400

    Update optim_utils.py

--- npt/utils/optim_utils.py
+++ npt/utils/optim_utils.py
@@ -15,22 +15,31 @@ Paper:
 https://arxiv.org/abs/1907.08610
 """
 
-import collections
+import itertools as it
+from torch.optim import Optimizer, Adam
+""" Lookahead Optimizer Wrapper.
+Implementation modified from: https://github.com/alphadl/lookahead.pytorch
+Paper: `Lookahead Optimizer: k steps forward, 1 step back` - https://arxiv.org/abs/1907.08610
+"""
+import torch
+from torch.optim.optimizer import Optimizer
 from collections import defaultdict
 
-import torch
-from torch.optim import Adam
-from torch.optim.optimizer import Optimizer
-from torch.utils.tensorboard import SummaryWriter
-
-
 class Lookahead(Optimizer):
+    def __init__(self, base_optimizer,alpha=0.5, k=6):
     def __init__(self, base_optimizer, alpha=0.5, k=6):
-        super().__init__()
         if not 0.0 <= alpha <= 1.0:
             raise ValueError(f'Invalid slow update rate: {alpha}')
         if not 1 <= k:
             raise ValueError(f'Invalid lookahead steps: {k}')
+        self.optimizer = base_optimizer
+        self.param_groups = self.optimizer.param_groups
+        self.alpha = alpha
+        self.k = k
+        for group in self.param_groups:
+            group["step_counter"] = 0
+        self.slow_weights = [[p.clone().detach() for p in group['params']]
+                                for group in self.param_groups]
         defaults = dict(lookahead_alpha=alpha, lookahead_k=k, lookahead_step=0)
         self.base_optimizer = base_optimizer
         self.param_groups = self.base_optimizer.param_groups
@@ -54,11 +63,28 @@ class Lookahead(Optimizer):
             slow.add_(group['lookahead_alpha'], fast_p.data - slow)
             fast_p.data.copy_(slow)
 
+        for w in it.chain(*self.slow_weights):
+            w.requires_grad = False
     def sync_lookahead(self):
         for group in self.param_groups:
             self.update_slow(group)
 
     def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+        loss = self.optimizer.step()
+        for group,slow_weights in zip(self.param_groups,self.slow_weights):
+            group['step_counter'] += 1
+            if group['step_counter'] % self.k != 0:
+                continue
+            for p,q in zip(group['params'],slow_weights):
+                if p.grad is None:
+                    continue
+                q.data.add_(self.alpha,p.data - q.data)
+                p.data.copy_(q.data)
+        # print(self.k)
+        #assert id(self.param_groups) == id(self.base_optimizer.param_groups)
         loss = self.base_optimizer.step(closure)
         for group in self.param_groups:
             group['lookahead_step'] += 1
@@ -106,122 +132,5 @@ class Lookahead(Optimizer):
                 for group in self.param_groups:
                     group.setdefault(name, default)
 
-
 def LookaheadAdam(params, alpha=0.5, k=6, *args, **kwargs):
-    adam = Adam(params, *args, **kwargs)
-    return Lookahead(adam, alpha, k)
-
-
-def log_lamb_rs(optimizer: Optimizer, event_writer: SummaryWriter, token_count: int):
-    """Log a histogram of trust ratio scalars in across layers."""
-    results = collections.defaultdict(list)
-    for group in optimizer.param_groups:
-        for p in group['params']:
-            state = optimizer.state[p]
-            for i in ('weight_norm', 'adam_norm', 'trust_ratio'):
-                if i in state:
-                    results[i].append(state[i])
-
-    for k, v in results.items():
-        event_writer.add_histogram(f'lamb/{k}', torch.tensor(v), token_count)
-
-
-class Lamb(Optimizer):
-    r"""Implements Lamb algorithm.
-    It has been proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes`_.
-    Arguments:
-        params (iterable): iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr (float, optional): learning rate (default: 1e-3)
-        betas (Tuple[float, float], optional): coefficients used for computing
-            running averages of gradient and its square (default: (0.9, 0.999))
-        eps (float, optional): term added to the denominator to improve
-            numerical stability (default: 1e-8)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-        adam (bool, optional): always use trust ratio = 1, which turns this into
-            Adam. Useful for comparison purposes.
-    .. _Large Batch Optimization for Deep Learning: Training BERT in 76 minutes:
-        https://arxiv.org/abs/1904.00962
-    """
-
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6,
-                 weight_decay=0, adam=False):
-        if not 0.0 <= lr:
-            raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0.0 <= eps:
-            raise ValueError("Invalid epsilon value: {}".format(eps))
-        if not 0.0 <= betas[0] < 1.0:
-            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
-        if not 0.0 <= betas[1] < 1.0:
-            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
-        defaults = dict(lr=lr, betas=betas, eps=eps,
-                        weight_decay=weight_decay)
-        self.adam = adam
-        super(Lamb, self).__init__(params, defaults)
-
-    # @profile
-    def step(self, closure=None):
-        """Performs a single optimization step.
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-        loss = None
-        if closure is not None:
-            loss = closure()
-
-        for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None:
-                    continue
-                grad = p.grad.data
-                if grad.is_sparse:
-                    raise RuntimeError('Lamb does not support sparse gradients, consider SparseAdam instad.')
-
-                state = self.state[p]
-
-                # State initialization
-                if len(state) == 0:
-                    state['step'] = 0
-                    # Exponential moving average of gradient values
-                    state['exp_avg'] = torch.zeros_like(p.data)
-                    # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
-
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-                beta1, beta2 = group['betas']
-
-                state['step'] += 1
-
-                # Decay the first and second moment running average coefficient
-                # m_t
-                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-                # v_t
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
-
-                # Paper v3 does not use debiasing.
-                # bias_correction1 = 1 - beta1 ** state['step']
-                # bias_correction2 = 1 - beta2 ** state['step']
-                # Apply bias to lr to avoid broadcast.
-                step_size = group['lr'] # * math.sqrt(bias_correction2) / bias_correction1
-
-                weight_norm = p.data.pow(2).sum().sqrt().clamp(0, 10)
-
-                adam_step = exp_avg / exp_avg_sq.sqrt().add(group['eps'])
-                if group['weight_decay'] != 0:
-                    adam_step.add_(p.data, alpha=group['weight_decay'])
-
-                adam_norm = adam_step.pow(2).sum().sqrt()
-                if weight_norm == 0 or adam_norm == 0:
-                    trust_ratio = 1
-                else:
-                    trust_ratio = weight_norm / adam_norm
-                state['weight_norm'] = weight_norm
-                state['adam_norm'] = adam_norm
-                state['trust_ratio'] = trust_ratio
-                if self.adam:
-                    trust_ratio = 1
-
-                p.data.add_(adam_step, alpha=-step_size * trust_ratio)
-
-        return loss
+     adam = Adam(params, *args, **kwargs)
