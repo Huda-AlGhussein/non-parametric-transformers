6730742c49212bb097aa0f37c82918046ec1cfa2
Yaser <yaseralosh@outlook.com>
2024-09-09 23:22:45 +0400

    Integrated transfer learning methods with baselines training and testing script.

--- baselines_train_test.py
+++ baselines_train_test.py
@@ -23,6 +23,12 @@ from baselines.smo_optimizer import SVM
 from sklearn import svm
 from sklearn.calibration import CalibratedClassifierCV
 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.preprocessing import label_binarize
+
+from transfer_learning_methods import watanabe_transform, cruz_transform, knn_filter, \
+    he_method, herbold_method, ma_transfer_naive_bayes, get_split_train_dataset
 MAX_ITER = 1000
 SKLEARN_CLASSREG_MODELS = {
     'XGBoost': {
@@ -54,18 +60,29 @@ SKLEARN_CLASSREG_MODELS = {
     },
     'SVM': {
         'class': lambda verbose: CalibratedClassifierCV(
-            svm.LinearSVC(verbose=verbose,C=0.1,max_iter=MAX_ITER))  #svm.SVC(kernel='linear',probability=True,verbose=True) # #SVM
+            svm.LinearSVC(verbose=True,C=0.1,max_iter=MAX_ITER))  #svm.SVC(kernel='linear',probability=True,verbose=True) # #SVM
     },
     'C45': {
         'class': lambda verbose: C45()
     },
     'NaiveBayes': {
-        'class': GaussianNB
+        'class': lambda verbose: GaussianNB()
     }
+}
+
+TRANSFER_LEARNING_METHODS = {
+    'watanabe'  : lambda x: watanabe_transform,
+    'cruz'      : lambda x: cruz_transform,
+    'turhan'    : lambda x: knn_filter,
+    'he_method' : lambda x: he_method,
+    'herbold'   : lambda x: herbold_method,
+    'ma_nb'     : lambda x: ma_transfer_naive_bayes
+    
 }
 
 DEFAULT_TARGET_COL = "label"
 DEFAULT_INPUT_FEATURES = lambda x: x[4:-1]
+
 HYPER_PARAMS_SEARCH = {
     'RandomForest':
         {
@@ -131,21 +148,22 @@ def save_results_to_csv(project_folder, model_name, task_type, test_results):
 def load_data(file_path):
     """Load data from a CSV file."""
     print(f"Load file {file_path}")
-    return pd.read_csv(file_path)
+    return pd.read_csv(file_path,index_col=0)
 
-def preprocess_data(data, target_column=DEFAULT_TARGET_COL):
+def preprocess_data(data, target_column=DEFAULT_TARGET_COL,scaler=True):
     """Preprocess the data."""
     #print(data.iloc[0])
     #print(data.columns)
     X = data.drop(columns=[target_column])
-    X = X.iloc[:,4:-1] #data.apply(DEFAULT_INPUT_FEATURES,axis=1)
+    X = X.iloc[:,3:-1] #data.apply(DEFAULT_INPUT_FEATURES,axis=1)
     #print(X.iloc[0])
     y = data[target_column]
-    
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-    
-    return X_scaled, y, scaler
+    if scaler:
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
+        
+        return X_scaled, y
+    return X,y
 
 def train_model(model, X_train, y_train, params=None):
     """Train a model with optional parameters."""
@@ -154,13 +172,10 @@ def train_model(model, X_train, y_train, params=None):
     model.fit(X_train, y_train)
     return model
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.preprocessing import label_binarize
-
-def evaluate_model(model, X_test, y_test, task_type):
+def evaluate_model(model, X_test, y_test, task_type, y_pred=None, y_proba=None):
     """Evaluate the model and return comprehensive metrics."""
-    y_pred = model.predict(X_test)
+    if y_pred == None:
+        y_pred = model.predict(X_test)
     
     if task_type == 'class':
         # Classification metrics
@@ -174,7 +189,9 @@ def evaluate_model(model, X_test, y_test, task_type):
         # Add ROC AUC score for binary classification
         #print(len(np.unique(y_test)))
         if len(np.unique(y_test)) == 2:
-            metrics['roc_auc'] = roc_auc_score(y_test, model.predict_proba(X_test)[:, 1])
+            if y_proba == None:
+                y_proba  = model.predict_proba(X_test)[:, 1]
+            metrics['roc_auc'] = roc_auc_score(y_test,y_proba)
         else:
             # For multi-class, calculate ROC AUC for each class
             y_test_bin = label_binarize(y_test, classes=np.unique(y_test))
@@ -192,6 +209,58 @@ def evaluate_model(model, X_test, y_test, task_type):
     
     return metrics
 
+def handle_transfer_learning(method_name, model, train_data, test_data, target_column):
+    """
+    Transforms the data according to the given method.
+
+    Returns: Modified_X_train, Y_train, Modified_X_test, Y_predictions
+    """
+    X_test, Y_test = preprocess_data(test_data)
+    if method_name not in TRANSFER_LEARNING_METHODS.keys(): 
+        X_train, y_train = preprocess_data(train_data, target_column)
+        print(f'ERROR: {method_name} does not exist as a transfer learning method')
+        return X_train, y_train, X_test, None, None
+    
+    if method_name == 'he_method' or method_name == 'herbold':
+
+        #method = TRANSFER_LEARNING_METHODS[method_name](0)
+        X_train_ps, Y_train_ps = get_split_train_dataset(train_data)
+        if method_name == 'he_method':
+            X_test, Y_test = preprocess_data(test_data, scaler=False)
+            final_predictions, final_probas = he_method(X_train_ps,X_test, Y_train_ps,model)
+            #print(final_predictions)
+            #print(final_probas)
+            return X_train_ps, Y_train_ps, X_test, final_predictions.tolist(),final_probas.tolist()
+        
+        else:
+
+            selected_projects, X_train_filtered, y_train_filtered =\
+                herbold_method(X_train_ps, Y_train_ps, X_test)
+
+            return X_train_filtered, y_train_filtered, X_test, None, None
+    
+    elif method_name == 'ma_nb':
+
+        X_train, y_train = preprocess_data(train_data, target_column)
+        final_predictions, proba = ma_transfer_naive_bayes(X_train,X_test,y_train)
+        return X_train, y_train, X_test, final_predictions.tolist(), proba.tolist()
+    
+    elif method_name == 'turhan':
+
+        X_train, y_train = preprocess_data(train_data, target_column)
+        method = TRANSFER_LEARNING_METHODS[method_name](0)
+        print(X_train.shape)
+        X_train_filtered,y_train = method(X_train, X_test,y_train)
+        print(X_train_filtered.shape)
+        return X_train_filtered,y_train, X_test, None, None
+    
+    else:
+
+        X_train, y_train = preprocess_data(train_data, target_column)
+        method = TRANSFER_LEARNING_METHODS[method_name](0)
+        X_test_modified = method(X_train, X_test)
+        return X_train, y_train, X_test_modified, None, None
+
 def perform_grid_search(model, X_train, y_train, param_grid, task_type):
     """Perform grid search for hyperparameter tuning."""
     grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy' if task_type=='class' else 'neg_mean_squared_error')
@@ -204,21 +273,22 @@ def evaluate_on_test_files(model, test_folder, task_type):
     for file in os.listdir(test_folder):
         if file.endswith('.csv'):
             test_data = load_data(os.path.join(test_folder, file))
-            X_test, y_test, _ = preprocess_data(test_data)
+            X_test, y_test = preprocess_data(test_data)
             performance = evaluate_model(model, X_test, y_test, task_type)
             results[file] = performance
     return results
 
-def process_project(project_folder, model_name, task_type, target_col=DEFAULT_TARGET_COL, grid_search=False):
+def process_project(project_folder, model_name, task_type,
+                     target_col=DEFAULT_TARGET_COL, grid_search=False):
     """Process a single project folder."""
     train_file = [f for f in os.listdir(project_folder) if f.endswith('.csv')][0]
     train_data = load_data(os.path.join(project_folder, train_file))
     
-    X_train, y_train, scaler = preprocess_data(train_data, target_col)
+    X_train, y_train = preprocess_data(train_data, target_col)
     #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     # experiment with smaller data
-    X_train = X_train[:10000,:]
-    y_train = y_train[:10000]
+    #X_train = X_train[:10000,:]
+    #y_train = y_train[:10000]
     model_class = SKLEARN_CLASSREG_MODELS[model_name][task_type]
     model = model_class(verbose=True)
     #print(f'ROC_AUC: {roc_auc_score(y_train, model.predict_proba(X_train)[:, 1])}')
@@ -258,7 +328,68 @@ def process_project(project_folder, model_name, task_type, target_col=DEFAULT_TA
     
     return model, test_results
 
-def main(base_folder, model_name, task_type, grid_search=False, specific_project=None):
+def process_project_with_transfer_learning(project_folder, model_name, task_type,transfer_learning_method,
+                     target_col=DEFAULT_TARGET_COL, grid_search=False):
+    """Process a single project folder."""
+    train_file = [f for f in os.listdir(project_folder) if f.endswith('.csv')][0]
+    train_data = load_data(os.path.join(project_folder, train_file))
+    
+    model_class = SKLEARN_CLASSREG_MODELS[model_name][task_type]
+    model = model_class(verbose=True)
+    test_folder = os.path.join(project_folder, 'test')
+    test_results = {}
+    for test_file in os.listdir(test_folder):
+        if not test_file.endswith('.csv'): continue
+        
+        test_data = load_data(os.path.join(test_folder, test_file))
+        
+        X_train, y_train, X_test, y_pred, y_probs = handle_transfer_learning(
+            transfer_learning_method,model, train_data,test_data,target_col)
+        #print(type(y_pred))
+        
+        if y_pred != None:
+            print(y_pred)
+            performance = evaluate_model(model, X_test, test_data[target_col], task_type,
+                                          y_pred=y_pred, y_proba=y_probs)
+            test_results[test_file] = performance
+            continue
+        #X_train, y_train, scaler = preprocess_data(train_data, target_col)
+        if grid_search:
+            # Define a simple param_grid for demonstration
+            if model_name in HYPER_PARAMS_SEARCH:
+                param_grid = HYPER_PARAMS_SEARCH[model_name]
+            else:
+                param_grid = {
+                    'n_estimators': [100, 200],
+                    'max_depth': [5, 10]
+                }
+            best_params = perform_grid_search(model, X_train, y_train, param_grid, task_type)
+            model = train_model(model, X_train, y_train, best_params)
+        
+        else:
+            model = train_model(model, X_train, y_train)
+        
+        performance = evaluate_model(model, X_test, test_data[target_col], task_type)
+        test_results[test_file] = performance
+    #performance = evaluate_model(model, X_test, y_test,task_type)
+    #test_results  = evaluate_on_test_files(model, test_folder,task_type)
+    # Log to wandb
+    if use_wandb:
+        for test_file, performance in test_results.items():
+            wandb.log({
+                'project': os.path.basename(project_folder),
+                'model': model_name,
+                'task_type': task_type,
+                'test_file': test_file,
+                **performance
+            })
+    
+    # Save results to CSV
+    save_results_to_csv(project_folder, model_name, f'{task_type}_{transfer_learning_method}', test_results)
+    
+    return model, test_results
+
+def main(base_folder, model_name, task_type, grid_search=False, specific_project=None, transfer_learning=None):
     """Main function to process all projects or a specific project in the base folder."""
     if use_wandb:
         wandb.init(project=f"cross_project_defect{specific_project if specific_project != None else ''}", 
@@ -271,30 +402,52 @@ def main(base_folder, model_name, task_type, grid_search=False, specific_project
                 })
     
     results = {}
-    
     if specific_project:
-        project_folder = os.path.join(base_folder, specific_project)
-        if os.path.isdir(project_folder):
-            print(f"Processing project: {specific_project}")
-            model, test_results = process_project(project_folder,
-            model_name, task_type,grid_search=grid_search)
-            results[specific_project] = {
+        projects_to_process = [os.path.join(base_folder, specific_project)]
+    else:
+        projects_to_process = [os.path.join(base_folder, p) for p in os.listdir(base_folder)]
+    for pf in projects_to_process:
+        if os.path.isdir(pf):
+            print(f"Processing project: {pf}")
+            if transfer_learning != None:
+                model,test_results = process_project_with_transfer_learning(
+                    pf, model_name, task_type, transfer_learning,grid_search=grid_search)
+            else:
+                model, test_results = process_project(pf,
+                    model_name, task_type,grid_search=grid_search)
+            results[pf] = {
                 'model': model,
                 'test_results': test_results
             }
         else:
-            print(f"Error: Project folder '{specific_project}' not found in '{base_folder}'")
-    else:
-        for project in os.listdir(base_folder):
-            project_folder = os.path.join(base_folder, project)
-            if os.path.isdir(project_folder):
-                print(f"Processing project: {project}")
-                model, test_results = process_project(project_folder,
-                model_name, task_type, grid_search=grid_search)
-                results[specific_project] = {
-                    'model': model,
-                    'test_results': test_results
-                }
+            print(f"Error: Project folder '{pf}' not found in '{base_folder}'")
+    # if specific_project:
+    #     project_folder = os.path.join(base_folder, specific_project)
+    #     if os.path.isdir(project_folder):
+    #         print(f"Processing project: {specific_project}")
+    #         if transfer_learning != None:
+    #             model,test_results = process_project_with_transfer_learning(
+    #                 project_folder, model_name, task_type, transfer_learning,grid_search=grid_search)
+    #         else:
+    #             model, test_results = process_project(project_folder,
+    #                 model_name, task_type,grid_search=grid_search)
+    #         results[specific_project] = {
+    #             'model': model,
+    #             'test_results': test_results
+    #         }
+    #     else:
+    #         print(f"Error: Project folder '{specific_project}' not found in '{base_folder}'")
+    # else:
+    #     for project in os.listdir(base_folder):
+    #         project_folder = os.path.join(base_folder, project)
+    #         if os.path.isdir(project_folder):
+    #             print(f"Processing project: {project}")
+    #             model, test_results = process_project(project_folder,
+    #             model_name, task_type, grid_search=grid_search)
+    #             results[specific_project] = {
+    #                 'model': model,
+    #                 'test_results': test_results
+    #             }
     
     if use_wandb: wandb.finish()
     
@@ -305,6 +458,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train and evaluate machine learning models on multiple projects.")
     parser.add_argument("--base_folder", type=str, default="Train-Test Data", help="Base folder containing project directories")
     parser.add_argument("--model", type=str, default="RandomForest", choices=SKLEARN_CLASSREG_MODELS.keys(), help="Model to use for training")
+    parser.add_argument("-tl","--transfer_learning", type=str, default=None, choices=TRANSFER_LEARNING_METHODS.keys(), help="Model to use for training")
     parser.add_argument("--task", type=str, default="class", choices=["class", "reg"], help="Task type: 'class' for classification, 'reg' for regression")
     parser.add_argument("--grid_search", action="store_true", help="Perform grid search for hyperparameter tuning")
     parser.add_argument("--project", type=str, help="Specific project to process (if not specified, all projects will be processed)")
@@ -314,7 +468,7 @@ if __name__ == "__main__":
 
     if args.wandb:
         use_wandb=True
-    results = main(args.base_folder, args.model, args.task, args.grid_search,args.project)
+    results = main(args.base_folder, args.model, args.task, args.grid_search,args.project,args.transfer_learning)
     for project, data in results.items():
         print(f"\nResults for project: {project}")
         for test_file, performance in data['test_results'].items():
---
+++ transfer_learning_methods.py
@@ -0,0 +1,260 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold 
+from sklearn.feature_selection import mutual_info_classif
+
+def get_project_indices(df, project_column='Project'):
+    # Group the dataframe by the project column and get the indices
+    grouped = df.groupby(project_column)
+    project_indices = {name: group.index for name, group in grouped}
+    return project_indices
+
+def split_dataframe_by_project_indices(df, project_indices):
+    # Create a dictionary to store each project's dataframe
+    project_dfs = [{'name':name,'df':df.loc[indices]} for name, indices in project_indices.items()]
+    return project_dfs
+
+def get_split_train_dataset (train_data):
+    #X_train = pd.read_csv(train_path, index_col=0)
+    #X_test = pd.read_csv(test_path, index_col=0)
+    #X_test = pd.read_csv('Train-Test Data/ant/Test/ant_test_1.csv', index_col=0)
+    Y_train = train_data["label"]
+    train_data = train_data.drop(columns=["bugs","label"])
+    #X_test.drop(columns=["bugs","label"],inplace=True)
+
+    X_train_indices = get_project_indices(train_data)
+    X_train = train_data.iloc[:,3:]
+    #scaler = StandardScaler()
+    #X_scaled = scaler.fit_transform(X_train)
+    #X_test = X_test.iloc[:,3:]
+    X_train_ps = split_dataframe_by_project_indices(X_train,X_train_indices)
+    Y_train_ps = split_dataframe_by_project_indices(Y_train,X_train_indices)
+
+    
+    return X_train_ps, Y_train_ps
+
+
+def watanabe_transform(X_train, X_test):
+    #print(X_train)
+    mean_train = X_train.mean()
+    mean_test = X_test.mean()
+    X_test_transformed = X_test * (mean_train / mean_test)
+    return X_test_transformed
+
+def cruz_transform(X_train, X_test):
+    #print(X_train)
+    median_train = np.median(X_train,axis=0) #X_train.median()
+    median_test = np.median(X_test,axis=0)#X_test.median()
+    X_test_transformed = X_test + (median_train - median_test)
+    return X_test_transformed
+
+def knn_filter(X_train, X_test, y_train, k=10):
+    knn = NearestNeighbors(n_neighbors=k, metric='euclidean')
+    knn.fit(X_train)
+    distances, indices = knn.kneighbors(X_test)
+    # To get the filtered training set without duplicates
+    #X_train_filtered = X_train[np.unique(indices.flatten())]
+    #X_train_filtered = np.unique(X_train[indices.flatten()], axis=0)
+    indices = np.unique(indices.flatten())
+    X_train_filtered = X_train[indices]
+    Y_train_filtered = y_train.iloc[indices]
+    return X_train_filtered,Y_train_filtered
+
+def he_method(X_train_projects, X_test, y_train_projects,model, N=10, FSS=0.8):
+    distances = []
+    SAMS=[]
+    # Step 1: Calculate distances between each training set and the test set
+    for i in range(len(X_train_projects)):
+        p_df = X_train_projects[i]['df']
+        p_name = X_train_projects[i]['name']
+        print(f'Processing {p_name} Project')
+        K = min(500,len(p_df), len(X_test))
+        SAMtrain = p_df.sample(n=K, random_state=42)
+        #SAMtest = X_test[np.random.randint(X_test.shape[0], size=K), :]
+        SAMtest = X_test.sample(n=K, random_state=42)
+        
+        SAMtrain['label'] = 1
+        SAMtest['label'] = -1
+        
+        SAM = pd.concat([SAMtrain, SAMtest])
+        SAMS.append(SAM)
+        y_SAM = SAM['label']
+        X_SAM = SAM.drop('label', axis=1)
+        kf = KFold(n_splits=5)
+        classifier = LogisticRegression(random_state=42)
+        classifier.fit(X_SAM, y_SAM)
+        
+        accuracy = accuracy_score(y_SAM, classifier.predict(X_SAM))
+        distances.append(2 * (accuracy - 0.5))  # Use 1 - accuracy as distance measure
+    # Step 2: Select top_N training sets with lowest distance
+    top_N_indices = np.argsort(distances)[:N]
+    selected_train_sets = [X_train_projects[i]['df'] for i in top_N_indices]
+    selected_train_labels = [y_train_projects[i]['df'] for i in top_N_indices]
+
+    # Step 3: Remove unstable features using Information Gain
+    def remove_unstable_features(X, y, ratio):
+        # Calculate information gain for each feature
+        info_gains = mutual_info_classif(X, y)
+        
+        # Sort features by information gain (descending order)
+        sorted_features = pd.Series(info_gains, index=X.columns).sort_values(ascending=False)
+        
+        # Select stable features (those with lower information gain)
+        n_keep = int(len(sorted_features) * (1 - ratio))
+        stable_features = sorted_features.index[-n_keep:]
+        
+        return X[stable_features]
+    
+    # Apply feature selection to each training set (maybe SAMS should be passed here instead, not sure)
+    selected_train_sets = [remove_unstable_features(X, y, FSS) 
+                           for X, y in zip(selected_train_sets, selected_train_labels)]
+    
+    # Apply feature selection to test set (using the first training set for consistency)
+    #X_test_stable = remove_unstable_features(X_test, y_train_projects[top_N_indices[0]]['df'], FSS)
+    # Step 4: Learn prediction models and ensemble
+    #cls_predictions=[]
+    
+    print(f'Predicting with {type(model).__name__} classifier..')
+    predictions , probabilities = [], []
+    for X_train, y_train in zip(selected_train_sets, selected_train_labels):
+        #model = model(random_state=42)
+        model.fit(X_train, y_train)
+        # Use same features as X_train
+        X_test_stable = X_test[X_train.columns] 
+        pred = model.predict(X_test_stable)
+        proba = model.predict_proba(X_test_stable)[:, 1]
+        predictions.append(pred)
+        probabilities.append(proba)
+    
+    # Calculate average prediction scores
+    predictions_array = np.array(predictions)
+    final_predictions = np.apply_along_axis(
+        lambda x: np.argmax(np.bincount(x)),
+        axis=0,
+        arr=predictions_array
+    )
+    final_probabilities = np.mean(probabilities, axis=0)
+    #cls_predictions.append({'model':type(cls).__name__,'final_predictions':final_predictions})
+        
+    return final_predictions,final_probabilities
+
+def he_method_multiple(X_train_projects, X_test, y_train_projects,classifiers=[RandomForestClassifier], N=10, FSS=0.8):
+    distances = []
+    SAMS=[]
+    # Step 1: Calculate distances between each training set and the test set
+    for i in range(len(X_train_projects)):
+        p_df = X_train_projects[i]['df']
+        p_name = X_train_projects[i]['name']
+        print(f'Processing {p_name} Project')
+        K = min(500,len(p_df), len(X_test))
+        SAMtrain = p_df.sample(n=K, random_state=42)
+        SAMtest = X_test.sample(n=K, random_state=42)
+        
+        SAMtrain['label'] = 1
+        SAMtest['label'] = -1
+        
+        SAM = pd.concat([SAMtrain, SAMtest])
+        SAMS.append(SAM)
+        y_SAM = SAM['label']
+        X_SAM = SAM.drop('label', axis=1)
+        kf = KFold(n_splits=5)
+        classifier = LogisticRegression(random_state=42)
+        classifier.fit(X_SAM, y_SAM)
+        
+        accuracy = accuracy_score(y_SAM, classifier.predict(X_SAM))
+        distances.append(2 * (accuracy - 0.5))  # Use 1 - accuracy as distance measure
+    # Step 2: Select top_N training sets with lowest distance
+    top_N_indices = np.argsort(distances)[:N]
+    selected_train_sets = [X_train_projects[i]['df'] for i in top_N_indices]
+    selected_train_labels = [y_train_projects[i]['df'] for i in top_N_indices]
+
+    # Step 3: Remove unstable features using Information Gain
+    def remove_unstable_features(X, y, ratio):
+        # Calculate information gain for each feature
+        info_gains = mutual_info_classif(X, y)
+        
+        # Sort features by information gain (descending order)
+        sorted_features = pd.Series(info_gains, index=X.columns).sort_values(ascending=False)
+        
+        # Select stable features (those with lower information gain)
+        n_keep = int(len(sorted_features) * (1 - ratio))
+        stable_features = sorted_features.index[-n_keep:]
+        
+        return X[stable_features]
+    
+    # Apply feature selection to each training set (maybe SAMS should be passed here instead, not sure)
+    selected_train_sets = [remove_unstable_features(X, y, FSS) 
+                           for X, y in zip(selected_train_sets, selected_train_labels)]
+    
+    # Apply feature selection to test set (using the first training set for consistency)
+    #X_test_stable = remove_unstable_features(X_test, y_train_projects[top_N_indices[0]]['df'], FSS)
+    # Step 4: Learn prediction models and ensemble
+    cls_predictions=[]
+    for cls in classifiers:
+        print(f'Predicting with {cls} classifier..')
+        predictions = []
+        for X_train, y_train in zip(selected_train_sets, selected_train_labels):
+            model = cls(random_state=42)
+            model.fit(X_train, y_train)
+            # Use same features as X_train
+            X_test_stable = X_test[X_train.columns] 
+            pred = model.predict_proba(X_test_stable)[:, 1]
+            predictions.append(pred)
+    
+        # Calculate average prediction scores
+        final_predictions = np.mean(predictions, axis=0)
+        cls_predictions.append({'model':type(cls).__name__,'final_predictions':final_predictions})
+        
+    return cls_predictions
+
+def herbold_method(X_train_projects,y_train_projects, X_test, k=0.5):
+    distances = []
+    SAMS=[]
+    X_test_mean = X_test.mean(axis=0)
+    X_test_std = X_test.std(axis=0)
+    distances = []
+    
+    for i in range(len(X_train_projects)):
+        p_df = X_train_projects[i]['df']
+        p_name = X_train_projects[i]['name']
+
+        project_means = p_df.mean(axis=0)
+        project_stds = p_df.std(axis=0)
+
+        sim = np.sqrt(((project_means - X_test_mean) ** 2).mean() + ((project_stds - X_test_std) ** 2).mean())
+        #print(sim)
+        distances.append(sim)
+    
+    indices = np.argsort(distances)
+    selected_projects = [X_train_projects[i] for i in indices]
+    
+    X_train_filtered = pd.concat([s['df'] for s in selected_projects])
+    y_train_filtered = pd.concat([y_train_projects[i]['df'] for i in indices])
+    
+    return selected_projects, X_train_filtered, y_train_filtered
+
+def ma_transfer_naive_bayes(X_train, X_test, y_train):
+    def calculate_weights(X_train, X_test):
+        weights = []
+        #for i, row in X_train.iterrows():
+        for row in X_train:
+            h = ((X_test.min() <= row) & (row <= X_test.max())).astype(int)
+            si = h.sum()
+            weight = 1 / ((len(row) - si + 1) ** 2)
+            weights.append(weight)
+        return np.array(weights)
+
+    weights = calculate_weights(X_train, X_test)
+    clf = GaussianNB()
+    clf.fit(X_train, y_train, sample_weight=weights)
+    predictions = clf.predict(X_test)
+    proba = clf.predict_proba(X_test)[:, 1]
+
+    return predictions,proba
